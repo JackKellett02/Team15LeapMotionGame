3808ad635ea185dde4bd420e6d8971b9a8e1d961
Jack Kellett <jackkellett02@gmail.com>
2021-10-28 12:09:15 +0100

    V1 of palmForceScript

--- team15LeapMotionGame/Assets/Scripts/palmForcescript.cs
+++ team15LeapMotionGame/Assets/Scripts/palmForcescript.cs
@@ -2,7 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class palmForcescript : MonoBehaviour
-{
-    void onTriggerEnter()
+public class palmForcescript : MonoBehaviour {
+	[SerializeField] 
+	private float force = 10.0f;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Football") {
+			Vector3 dirToFootball = other.gameObject.transform.position - gameObject.transform.position;
+			dirToFootball.Normalize();
+			dirToFootball = dirToFootball * force;
+			Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
+			rigidbody.AddForce(dirToFootball, ForceMode.Impulse);
+		}
+	}
 }
