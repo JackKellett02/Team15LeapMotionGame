fef30d30e901e5165a03cb2fdcda3dc45d792e47
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2021-11-18 12:57:49 +0000

    target random range

---
+++ team15LeapMotionGame/Assets/Scripts/goalMover.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goalMover : MonoBehaviour
+{
+    GameObject target;
+    //void targetSpawn()
+    //{
+    //    Vector3 position = new Vector3(Random.Range(-2.303f, 1.446f),Random.Range(-0.878f, 0.557f), (5.015f));
+    //    Instantiate(target, position, Quaternion.identity);
+    //}
+    Vector3 startPos;
+    GameObject targetPlane;
+
+  
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Football")
+        {
+            Vector3 position = new Vector3(Random.Range(-1.687f, 1.021f), Random.Range(-0.535f, 0.07f), startPos.z);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
