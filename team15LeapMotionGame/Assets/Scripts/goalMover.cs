1788ab8a1d1599c53b032072429041d606c3c3b0
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2021-11-25 11:29:12 +0000

    goal moves back on score and made ground bigger to accommodate

--- team15LeapMotionGame/Assets/Scripts/goalMover.cs
+++ team15LeapMotionGame/Assets/Scripts/goalMover.cs
@@ -5,29 +5,38 @@ using UnityEngine;
 public class goalMover : MonoBehaviour
 {
     GameObject target;
-    //void targetSpawn()
-    //{
-    //    Vector3 position = new Vector3(Random.Range(-2.303f, 1.446f),Random.Range(-0.878f, 0.557f), (5.015f));
-    //    Instantiate(target, position, Quaternion.identity);
-    //}
     Vector3 startPos;
     GameObject targetPlane;
 
-  
+    int counter = 1;
+    
+    [SerializeField] 
+    GameObject GoalObject;
+
+    [SerializeField]
+    float goalMovement = 2.0f;
+
+
+
 
     private void Start()
     {
         startPos = transform.position;
     }
 
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Football")
         {
+            counter++;
             Vector3 position = new Vector3(Random.Range(-1.687f, 1.021f), Random.Range(-0.535f, 0.07f), startPos.z);
             transform.position = position;
+
+            Vector3 goalPos = new Vector3(-0.37f, -0.181f, 5.39f);
+            goalPos.z += goalMovement * counter;
+            GoalObject.transform.position = goalPos;
         }
+        
     }
 
 
