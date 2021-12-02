386a335bc2fe52a9658ee7e2bae85379e536f6a4
Jack Kellett <jackkellett02@gmail.com>
2021-12-02 12:00:33 +0000

    Update goalMover.cs

--- team15LeapMotionGame/Assets/Scripts/goalMover.cs
+++ team15LeapMotionGame/Assets/Scripts/goalMover.cs
@@ -2,47 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class goalMover : MonoBehaviour
-{
-    GameObject target;
-    Vector3 startPos;
-    GameObject targetPlane;
+public class goalMover : MonoBehaviour {
+	GameObject target;
+	Vector3 startPos;
+	GameObject targetPlane;
 
-    int counter = 1;
-    
-    [SerializeField] 
-    GameObject GoalObject;
+	int counter = 1;
 
-    [SerializeField]
-    float goalMovement = 2.0f;
+	[SerializeField]
+	GameObject GoalObject;
 
+	[SerializeField]
+	float goalMovement = 2.0f;
 
 
 
-    private void Start()
-    {
-        startPos = transform.position;
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Football")
-        {
-            counter++;
-            Vector3 position = new Vector3(Random.Range(-1.687f, 1.021f), Random.Range(-0.535f, 0.07f), startPos.z);
-            transform.position = position;
+	private void Start() {
+		startPos = transform.position;
+	}
 
-            Vector3 goalPos = new Vector3(-0.37f, -0.181f, 5.39f);
-            goalPos.z += goalMovement * counter;
-            GoalObject.transform.position = goalPos;
-        }
-        
-    }
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Football") {
+			counter++;
+
+			Vector3 goalPos = new Vector3(-0.37f, -0.181f, 5.39f);
+			goalPos.z += goalMovement * counter;
+			GoalObject.transform.position = goalPos;
+
+			Vector3 position = new Vector3(Random.Range(-1.687f, 1.021f), Random.Range(-0.535f, 0.07f), goalPos.z);
+			transform.position = position;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		}
+
+	}
+
+
+	// Update is called once per frame
+	void Update() {
+
+	}
 }
